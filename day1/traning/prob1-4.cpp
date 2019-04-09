0cfc9c138feb3fc6c2a51e094362ada48085525d
yuyabu <yuyabushita@gmail.com>
2019-04-09 16:36:12 +0900

    演習問題4の回答
    初回時にコンパイルエラーがでたのでメモ
    
    prob1-4.cpp: In function ‘int main()’:
    prob1-4.cpp:10:59: error: invalid operands of types ‘const char*’ and ‘const char [10]’ to binary ‘operator+’
      cout << inputed + "を2倍した数は、" +(inputed * 2)+"です。"<<endl;
              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~

---
+++ day1/traning/prob1-4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int inputed;
+	cout << "数値を入力してください:";
+	cin >> inputed;
+	cout << inputed << "を2倍した数は、" << (inputed * 2) <<"です。"<<endl;
+}
