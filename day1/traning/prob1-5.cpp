6c5e91da0e1eb17d4f5e13471a6205ef59488838
yuyabu <yuyabushita@gmail.com>
2019-04-09 16:42:12 +0900

    演習問題5の回答

---
+++ day1/traning/prob1-5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int n1;
+	int n2;
+	cout << "2つめの数:";
+	cin >> n1;
+	cout << "2つめの数;";
+	cin >> n2;
+
+	cout << n1 << "+" << n2 << "=" << (n1 + n2) << endl;
+	cout << n1 << "-" << n2 << "=" << (n1 - n2) << endl;
+	return 0;
+}
