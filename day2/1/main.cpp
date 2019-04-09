13006890691e56a3777587a2bae3e3d06e5aa58f
yuyabu <yuyabushita@gmail.com>
2019-04-09 17:17:15 +0900

    最初のクラスサンプルのコードを追加
    コンパイルのコマンドに手間取った。クラスとメインクラスをコンパイラの引数にしていすればコンパイルできた。
    
    g++ main.cpp sample.cpp

---
+++ day2/1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "sample.h"
+
+using namespace std;
+
+int main()
+{
+	CSample obj; //CSampleをインスタンス化
+	int num;
+
+	cout << "整数を入力してください:" << endl;
+	cin >> num;
+
+	obj.set(num); //CSampleのメンバ変数をセット
+	cout << obj.get() << endl; //メンバ変数の値を出力
+
+	return 0;
+}
---
+++ day2/1/sample.h
@@ -0,0 +1,14 @@
+#ifndef _SAMPLE_H_
+#define _SAMPLE_H_
+
+//クラス宣言
+class CSample
+{
+	public:
+		void set(int num);
+		int get();
+	private:
+		int m_num;
+};
+
+#endif // _SAMPLE_H_
