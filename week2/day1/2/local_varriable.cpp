0be4bb59930c5b90f0513a85565e48eaf2d8b1b7
yuyabu <yuyabushita@gmail.com>
2019-04-10 20:22:20 +0900

    サンプルコード7-2の追加
    C++ではローカル変数がどこでも宣言できる、とあるけど、C言語も
    最近のはどこでも宣言できたような。

---
+++ week2/day1/2/local_varriable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "ABC" << endl;
+	// 処理の頭注で変数を宣言
+	int i;
+	for ( - 0; i< 10; i++){
+		cout << ":" << i;
+	}
+	cout << endl;
+	return 0;
+}
