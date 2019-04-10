012d913dbcfaae4ac35241ef1e6c5853161c5d1a
yuyabu <yuyabushita@gmail.com>
2019-04-10 15:53:25 +0900

    サンプル7-2の追加。
    書いてあるとおり、エラーになった
    
    main.cpp: In function ‘int main()’:
    main.cpp:6:18: error: no matching function for call to ‘Sample::Sample()’
      p2 = new Sample();
                      ^
    In file included from main.cpp:1:0:
    Sample.h:6:3: note: candidate: Sample::Sample(int)
       Sample(int a);
       ^~~~~~
    Sample.h:6:3: note:   candidate expects 1 argument, 0 provided
    Sample.h:4:7: note: candidate: constexpr Sample::Sample(const Sample&)
     class Sample{
           ^~~~~~
    Sample.h:4:7: note:   candidate expects 1 argument, 0 provided
    Sample.h:4:7: note: candidate: constexpr Sample::Sample(Sample&&)
    Sample.h:4:7: note:   candidate expects 1 argument, 0 provided

---
+++ day7/2/Sample.h
@@ -0,0 +1,9 @@
+#ifndef _SAMPLE_H_
+#define _SAMPLE_H_
+
+class Sample{
+	public:
+		Sample(int a);
+};
+
+#endif // _SAmPLE_H_
---
+++ day7/2/main.cpp
@@ -0,0 +1,8 @@
+#include "Sample.h"
+
+int main(){
+	Sample *p1,*p2;
+	p1= new Sample(1);
+	p2 = new Sample();
+	return 0;
+}
