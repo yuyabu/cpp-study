21f0e68dd25cf8a23cf1cfaf8c98613526f974eb
yuyabu <yuyabushita@gmail.com>
2019-04-11 15:42:18 +0900

    演習問題3の回答

--- week2/day1/traning/3/main.cpp
+++ week2/day1/traning/3/main.cpp
@@ -7,14 +7,14 @@ void swap(int *, int *);
 int main(){
 	int a = 1,b = 2;
 	cout << "a = " << a <<  " b = " << b << endl;
-	swap(&a, &b);
+	swap(a, b);
 	cout << "a = " << a << " b = " << b << endl;
 	return 0;
 }
 
-void swap(int *a, int *b){
+void swap(int &a, int &b){
 	int tmp;
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
 }
